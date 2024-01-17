d4a50df7842a7881da47036b084cbcff12487e1b
Sayandeep <sayandeep11@gmail.com>
2024-01-18 03:34:24 +0530

    login page frontend done

--- app/logindev/login.js
+++ app/logindev/login.js
@@ -124,32 +124,58 @@ export default function login(){
 
     return(
         <>
-        
-        {!otpSent ? (
-            <div id="recaptcha-container"></div>
-        ) : null}
+      
+        <section className="font-poppins ">
+        <div className="hidden py-20 text-center bg-blue-100   :bg-gray-700 lg:block">
+          <div className="max-w-3xl mx-auto mb-24">
+            <p className="mb-6 font-semibold text-gray-800 text-4xl   :text-gray-300">
+              Login our account
+            </p>
+            <p className="mb-6 text-xl text-gray-500 "></p>
+          </div>
+        </div>
+        <div className="max-w-xl mx-auto ">
+          <div className="w-full shadow-lg bg-gray-50   :bg-gray-800 mt-11 lg:-mt-36 lg:full p-7 rounded-3xl">
+            <div className="">
+              <div action="" className="p-0 m-0">
+                <div className="mb-7">
+                  <input
+                    type="tel"
+                    className="w-full px-4 py-4 bg-gray-200 rounded-lg   :bg-gray-700 lg:py-5   :text-gray-300 "
+                    value = {phoneNumber}
+                    onChange={handlePhoneNumberChange}
+                    placeholder="Enter your phone number"
+                  />
+                </div>
+                <div className="mb-6 flex items-center">
+                  <div className="relative flex items-center justify-between  ">
+                    <input
+                      type="tel"
+                      className="w-full px-4 py-3 bg-gray-200 rounded-lg lg:py-4     "
+                      value = {otp}
+                    onChange={handleOTPChange}
+                      placeholder="Enter OTP"
+                    />
 
-        <input 
-        type="tel" 
-        value = {phoneNumber}
-        onChange={handlePhoneNumberChange}
-        placeholder='enter the phone number'
-        />
-
-        <input 
-        type="text" 
-        value = {otp}
-        onChange={handleOTPChange}
-        placeholder='enter the OTP'
-        />
-
-        <button
-        onClick={otpSent ? handleOTPSubmit : handleSendOtp}
-        >
-            {otpSent ? 'submit OTP' : 'send OTP'}
-        </button>
-            
-
+                    
+                  </div>
+                </div>
+                <div> 
+                    {!otpSent ? (
+                    <div id="recaptcha-container"></div>
+                ) : null}
+                </div>
+                <button
+                  className="w-full px-4 py-4 mt-6 font-medium text-gray-200 bg-blue-700 rounded-lg   :bg-blue-500 hover:text-blue-200 "
+                  onClick={otpSent ? handleOTPSubmit : handleSendOtp}
+                >
+                  {otpSent ? 'submit OTP' : 'send OTP'}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
         
         </>
     )
